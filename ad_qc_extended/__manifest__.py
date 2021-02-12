c1108ff67b93c5b8925eca0a256efeb17f727359
Mayur Maheshwari <mayurmaheshwari07@gmail.com>
2021-02-12 15:34:09 +0530

    qc extended for mintues

---
+++ ad_qc_extended/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#	
+#		Mayur Maheshwari
+#
+##############################################################################
+{
+    'name': 'Quality Control Extended', 
+    'version': '13.0.1.0',
+    'sequence': 1, 
+    'category': 'Quality', 
+    'description':  
+        """ 
+        This module will help to extend quality controll flow.
+        
+    """,
+    'summary': 'Extend Quality Control', 
+    'author': 'Mayur Maheshwari', 
+    'website': 'Mayur Maheshwari',
+    'depends': ['quality_control'],
+    "data": [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+
